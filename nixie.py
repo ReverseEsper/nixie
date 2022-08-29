e6be3fb7062fad97f1cd80a96760bbe8988a5df2
Adam Kurowski <adam.kurowski@keyloop.com>
2022-08-29 12:35:17 +0200

    nixie class

--- nixie.py
+++ nixie.py
@@ -2,142 +2,104 @@ import RPi.GPIO as IO         # calling for header file which helps us use GPIO�
 import time                             # calling for time to provide delays in program
 import datetime   
 
-HV_pin = 14
-DATA_pin = 16
-CLOCK_pin = 21       
-SHOW_pin = 20
-WAIT_TIME = 0.00001
-VISIBLE = True
-TOUCH_pin = 2
-TOUCH_pin2 = 17
+class Nixie:
+    HV_pin = 14
+    DATA_pin = 16
+    CLOCK_pin = 21       
+    SHOW_pin = 20
+    WAIT_TIME = 0.00001
+    VISIBLE = True
 
-digit_table = {
-    0 : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    1 : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
-    2 : [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
-    3 : [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
-    4 : [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
-    5 : [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
-    6 : [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
-    7 : [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
-    8 : [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
-    9 : [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
-    10 :[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
-    11 :[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-}
+    digit_table = {
+        0 : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        1 : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+        2 : [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+        3 : [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+        4 : [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+        5 : [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+        6 : [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+        7 : [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+        8 : [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+        9 : [0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],
+        10 :[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+        11 :[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    }
 
+    def __init__(self)
+        IO.setwarnings(False)           # do not show any warnings
+        IO.setmode (IO.BCM)        # programming the GPIO by BCM pin numbers. (like PIN29 as‘GPIO5’)
+        IO.setup(self.DATA_pin,IO.OUT)            # initialize GPIO Pins as an output.
+        IO.setup(self.CLOCK_pin,IO.OUT)
+        IO.setup(self.SHOW_pin,IO.OUT)
+        IO.setup(self.HV_pin,IO.OUT)
 
-'''
-Timecheet stats :
-Max Clock : 4,2MHz @2V (21MHz@4.5V)
-Pulse Duration : min 120 ns
-SetUp Time : min 150ns
-Minimal time: time.sleep(1ms)
-'''
+    '''
+    Timecheet stats :
+    Max Clock : 4,2MHz @2V (21MHz@4.5V)
+    Pulse Duration : min 120 ns
+    SetUp Time : min 150ns
+    Minimal time: time.sleep(1ms)
+    '''
 
-IO.setwarnings(False)           # do not show any warnings
-IO.setmode (IO.BCM)        # programming the GPIO by BCM pin numbers. (like PIN29 as‘GPIO5’)
-IO.setup(DATA_pin,IO.OUT)            # initialize GPIO Pins as an output.
-IO.setup(CLOCK_pin,IO.OUT)
-IO.setup(SHOW_pin,IO.OUT)
-IO.setup(HV_pin,IO.OUT)
-IO.setup(TOUCH_pin,IO.IN)
-IO.setup(TOUCH_pin2,IO.IN)
+    def power_on(self):
+        #Put current to output
+        IO.output(self.HV_pin,1)
 
+    def power_off(self):
+        #Put current to output
+        IO.output(self.HV_pin,0)
 
-#Put current to output
-IO.output(HV_pin,1)
-
-
-def show():
-    IO.output(SHOW_pin,1)            # pull the SHIFT pin high to put the 8 bit data out parallel
-    time.sleep(WAIT_TIME)
-    IO.output(SHOW_pin,0)            # pull down the SHIFT pin
-
-def send_bit(bit):
-    IO.output(DATA_pin,bit)            # pull up the data pin for every bit.
-    time.sleep(WAIT_TIME)            # wait for 100ms
-    IO.output(CLOCK_pin,1)            # pull CLOCK pin high
-    time.sleep(WAIT_TIME)
-    IO.output(CLOCK_pin,0)            # pull CLOCK pin down, to send a rising edge
-    IO.output(DATA_pin,0)            # clear the DATA pin
+    def update_display(self):
+        IO.output(self.SHOW_pin,1)            # pull the SHIFT pin high to put the 8 bit data out parallel
+        time.sleep(self.WAIT_TIME)
+        IO.output(self.SHOW_pin,0)            # pull down the SHIFT pin
 
 
-def push_digit(character,dot=False):
-    digit_map = digit_table[character]
-    if dot:
-        digit_map[5]=1
-    else:
-        digit_map[5]=0
-        
+    def send_bit(self,bit):
+        IO.output(self.DATA_pin,bit)            # pull up the data pin for every bit.
+        time.sleep(self.WAIT_TIME)            # wait for 100ms
+        IO.output(self.CLOCK_pin,1)            # pull CLOCK pin high
+        time.sleep(self.WAIT_TIME)
+        IO.output(self.CLOCK_pin,0)            # pull CLOCK pin down, to send a rising edge
+        IO.output(self.DATA_pin,0)            # clear the DATA pin
 
-    for bit in digit_map:
-        send_bit(bit)
 
-def show_time(dot=False):
-    now = datetime.datetime.now()
-    min_last = now.minute%10
-    min_first = int(now.minute/10)
-    h_last = now.hour%10
-    h_first = int(now.hour/10)
-    #push_digit(int(now.minute/10),dot)
-    push_digit(h_first,dot)
-    push_digit(h_last,dot)
-    push_digit(min_first,dot)
-    push_digit(min_last,dot)
-    show()
-
-def show_dot(state):
-    push_digit(11)
-    push_digit(11,True)
-    push_digit(11,True)
-    push_digit(11)
-    show()
-
-#for i in range (0,10):
-#    push_digit(i)
-#    show()
-#    time.sleep(1)
-
-def clock():
-    global VISIBLE
-    blink = False
-    while True:
-        if blink:
-            blink = False
+    def push_digit(self,character,dot=False):
+        digit_map = self.digit_table[character]
+        if dot:
+            self.digit_map[5]=1
         else:
-            blink = True
-        # Sprawdzamy czas
+            self.digit_map[5]=0
+            
+
+        for bit in self.digit_map:
+            self.send_bit(bit)
+
+    def show_time(dot=False):
         now = datetime.datetime.now()
-        at23 = now.replace(hour=21,minute=0,second=0)
-        at05 = now.replace(hour=5,minute=0,second=0)
-        tp = IO.input(TOUCH_pin)
-        tp2 = IO.input(TOUCH_pin2)
-        if  now < at23 and  now > at05 or tp:
-            if not VISIBLE:
-                VISIBLE = True
-                IO.output(HV_pin,1)
-            if tp2:
-                VISIBLE = False
-                IO.output(HV_pin,0)
-        else:
-            if VISIBLE:
-                VISIBLE = False
-                IO.output(HV_pin,0)
+        min_last = now.minute%10
+        min_first = int(now.minute/10)
+        h_last = now.hour%10
+        h_first = int(now.hour/10)
+
+        self.push_digit(h_first,dot)
+        self.push_digit(h_last,dot)
+        self.push_digit(min_first,dot)
+        self.push_digit(min_last,dot)
+        self.update_display()
 
 
-
-        show_time(blink)
-        time.sleep(1)
-        print(VISIBLE)
+nixie  = Nixie()
+nixie.power_on()
 
 
-clock()
+blink = False
+while True:
+    if blink:
+        blink = False
+    else:
+        blink = True
+    nixie.show_time(blink)
+    time.sleep(1)
 
-'''
-A co potem ?
-
-1. Zegar
-2. Obsluga guzików dotykowych w dolnej obudowie
-3. Głośniczki Radiowe
-'''
+nixie.power_off()
