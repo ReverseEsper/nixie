2a392288d4a42510f1e8f3eb1e12d8b9937612a9
Adam Kurowski <adam.kurowski@keyloop.com>
2022-10-03 14:56:48 +0200

    Debian noninteractive

--- hours.py
+++ hours.py
@@ -1,10 +1,34 @@
-def en(a,b):
+import datetime
+mode =  "Zegar"
+display_table = {}
+display_table['Digit4'] = 0
+display_table['Digit3'] = 0
+display_table['Digit2'] = 0
+display_table['Digit1'] = 0
+
+def flashing(target,field):
+    global display_table
+    a = display_table[field]
+    b = target
     sign = -1 if a > b else 1
-    return range(a,b+sign,sign)
+    for i in range(a,b+sign,sign):
+        display_table[field] = i
+        print (f"i:{i}, field:{field}")
 
 
-a = 0
-b = 10
+def clock():
+    global display_table,mode
 
-for i in rever(a,b):
-    print(i)
+    if mode == "Zegar":
+        now = datetime.datetime.now()            
+        flashing(now.minute%10,'Digit4') 
+        flashing(int(now.minute/10),'Digit3') 
+        flashing(now.hour%10,'Digit2') 
+        flashing(int(now.hour/10),'Digit1') 
+        display_table['Dot'] = now.second%2
+
+
+clock()
+
+
+
